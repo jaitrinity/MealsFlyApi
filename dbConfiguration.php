684390addb4a3fc3c32af1693456be8a822d08c9
Jai Trinity <jai.prakash@trinityapplab.co.in>
2024-07-29 11:41:02 +0530

    revert

--- dbConfiguration.php
+++ dbConfiguration.php
@@ -2,7 +2,6 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers:content-type");
-$conn=mysqli_connect("[hostname]","[username]","[password]","[dbname_v3]");
-$conn=mysqli_connect("[hostname]","[username]","[password]","[dbname_v2]");
+$conn=mysqli_connect("[hostname]","[username]","[password]","[dbname]");
 mysqli_set_charset($conn, 'utf8');
 ?>
